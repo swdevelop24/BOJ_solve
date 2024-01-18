4088723ca8344e24d9a2e30e877d4af27588b005
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-01-18 14:19:04 -0600

    [Gold IV] Title: 부분합, Time: 32 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.cc
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -1,44 +1,37 @@
-#include <iostream>
-#include <algorithm>
-#include <string>
-typedef long long int ll;
+#include<iostream>
+#include<algorithm>
 using namespace std;
 
-#define NM 500005
+int n, s, ans;
+int arr[100000];
+int mini = 21e8;
+int main() {
 
-int N, S;
-int A[NM];
-
-void input() {
-	cin >> N >> S;
-	for (int i = 1; i <= N; i++) cin >> A[i];
-}
-
-void pro() {
-	int R = 0, sum = 0, ans = N + 1;
-	for (int L = 1; L <= N; L++) {
-		// L - 1 을 구간에서 제외하기
-		sum -= A[L - 1];
-
-		// R 을 옮길 수 있을 때 까지 옮기기
-		while (R + 1 <= N && sum < S)
-			sum += A[++R];
-
-		// [L ... R] 의 합, 즉 sum이 조건을 만족하면 정답 갱신하기
-		if (sum >= S)
-			ans = min(ans, R - L + 1);
+	cin >> n >> s;
+	ans = n + 1;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
 	}
 
-	// ans 값을 보고 불가능 판단하기
-	if (ans == N + 1)
-		ans = 0;
+	int left = 0;
+	int right = 0;
+	int subsum = 0;
+	int flag = 0;
+
+	for (int t = left; t < n; t++) {
+		while (subsum < s && right < n) {
+			subsum += arr[right];
+			right++;
+		}
+		if (subsum >= s)
+			ans = min(ans, right - left);
+		subsum -= arr[left];
+		left++;
+	}
+
+	if (ans == n + 1) ans = 0;
 	cout << ans;
-}
 
 
-int main() {
-	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-	input();
-	pro();
 	return 0;
 }
