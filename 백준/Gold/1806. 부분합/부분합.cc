9858aa6acd51dd4868601ca2a45f80de2dde1efc
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-05-30 13:45:35 -0500

    [Gold IV] Title: 부분합, Time: 32 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.cc
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -1,37 +1,38 @@
 #include<iostream>
-#include<cmath>
 using namespace std;
-
-int n, s; 
-int arr[100001]; 
 
 int main() {
 	//freopen_s(new FILE*, "a.txt", "r", stdin);
-	cin >> n >> s; 
+
+	int n, s; 
+	cin >> n >> s;
+
+	int arr[100001] = { 0 }; 
+
 	for (int i = 0; i < n; i++) {
 		cin >> arr[i]; 
 	}
-	int b = -1; 
-	int sum = 0;
-	int ret = n + 1; 
-	for (int a = 0; a < n; a++) {
-		while (b + 1 < n && sum < s) {
-			b++;
-			sum += arr[b]; 
+
+	int sum = arr[0];
+	int cnt = 0; 
+	int mincnt = n + 1; 
+
+	int left = 0;
+	int right = 0; 
+
+	for (left = 0; left < n; left++) {
+		while (right < n && sum < s) {
+			right++;
+			sum += arr[right]; 
 		}
 		if (sum >= s) {
-			ret = min(ret, b - a + 1); 
+			mincnt = min(mincnt, right - left +1); 
 		}
-		sum -= arr[a]; 
+		sum -= arr[left]; 
 	}
 
-	if (ret == n + 1) cout << 0;
-	else cout << ret;
-
+	if (mincnt == n + 1) cout << 0;
+	else cout << mincnt;
 
 	return 0;
 }
-
-
-
-
