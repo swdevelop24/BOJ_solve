21963d3e37771a0f1381486a7a65ee5beea8d9ff
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-05-30 15:28:00 -0500

    [Gold V] Title: 두 용액, Time: 16 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.cc
+++ 백준/Gold/2470. 두 용액/두 용액.cc
@@ -4,37 +4,43 @@
 using namespace std;
 
 int n;
-int arr[100001]; 
+int arr[100005];
+
 int main() {
+	//freopen_s(new FILE*, "a.txt", "r", stdin);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> n;
 
-	//freopen_s(new FILE*, "a.txt", "r", stdin); 
-
-	cin >> n; 
+	
 	for (int i = 0; i < n; i++) {
-		cin >> arr[i]; 
+		cin >> arr[i];
 	}
 
 	sort(arr, arr + n);
 
-	int minsum = 21e8; 
-	int sum = 0; 
-	int a = 0; 
-	int b = n - 1; 
-	int ret1 = 0, ret2 = 0; 
-	while (a < b) {
-		sum = arr[a] + arr[b];
-		if (minsum > abs(sum)) {
-			minsum = abs(sum);
-			ret1 = arr[a]; 
-			ret2 = arr[b]; 
- 		}
-		if (sum < 0) {
-			a++;
+
+	int left = 0;
+	int right = n - 1;
+	int minsum = 21e8;
+	int ret1, ret2; 
+	while (left < right) {
+		if (abs(arr[left] + arr[right]) < minsum) {
+			minsum = abs(arr[left] + arr[right]);
+			ret1 = arr[left];
+			ret2 = arr[right];
 		}
-		else b--;
+		if (arr[left] + arr[right] > 0) {
+			right--;
+		}
+		else {
+			left++;
+		}
 	}
-	
+
 	cout << ret1 << " " << ret2;
 
-	return 0; 
+
+	return 0;
 }
