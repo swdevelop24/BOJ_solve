44fd6ad5e7d37288c2a5dc96f011fdd80039b762
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-06-03 16:05:29 -0500

    [Gold IV] Title: List of Unique Numbers, Time: 12 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.cc
+++ 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.cc
@@ -2,27 +2,35 @@
 using namespace std;
 
 int n;
-int arr[100001];
-int dat[100001];
-long long cnt;
+int arr[100001]; 
+int cnt; 
+int dat[100001]; 
+long long ans;
 
 int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
 	//freopen_s(new FILE*, "a.txt", "r", stdin);
 
-	cin >> n;
+	cin >> n; 
+	
 	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
-	}
-	int right = 0;
-	for (int left = 0; left < n; left++) {
-		while (right < n && dat[arr[right]] < 1) {
-			dat[arr[right]]++;
-			right++;
-		}
-		cnt += right - left;
-		dat[arr[left]]--;
+		cin >> arr[i]; 
 	}
 
-	cout << cnt;
+	int right = -1;
+	for (int left = 0; left < n; left++) {
+		while ((dat[arr[right+1]] == 0) && right+1 <n) {
+			dat[arr[right+1]]++;
+			right++;
+		}
+		ans += right - left + 1; 
+		dat[arr[left]]--; 
+
+	}
+
+	cout << ans; 
 	return 0;
 }
