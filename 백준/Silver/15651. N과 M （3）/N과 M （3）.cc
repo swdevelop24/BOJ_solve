c6b5538af948b33c438645257a44f5c26dc4d008
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-08-29 12:28:01 -0500

    [Silver III] Title: N과 M (3), Time: 412 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -1,23 +1,26 @@
 #include<iostream>
 using namespace std;
-int n, m; 
-int path[8]; 
-void dfs(int lev) {
-	if (lev == m) {
-		for (int i = 1; i <= lev; i++) {
-			cout << path[i] << " ";
-		}
-		cout << "\n";
-		return; 
-	}
-	for (int i = 1; i <= n; i++) {
-		path[lev+1] = i;
-		dfs(lev + 1); 
-		path[lev+1] = 0;
-	}
+int n, m;
+int path[10]; 
+
+void run(int lev){
+    if(lev==m){
+        for(int i=0; i<lev; i++){
+            cout << path[i] <<" "; 
+        }        
+        cout << "\n"; 
+        return; 
+    }
+    
+    for(int i=1; i<=n;i++){
+        path[lev]=i; 
+        run(lev+1); 
+    }
 }
 
-int main() {
-	cin >> n >> m; 
-	dfs(0); 
+int main(){
+    cin>>n>>m; 
+    
+    run(0); 
+    return 0; 
 }
